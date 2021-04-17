aa5d1942857895c215ca403f783438e15865c846
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-04-17 15:55:28 -0300

    none na string

--- iot_temperatura_cpu.py
+++ iot_temperatura_cpu.py
@@ -5,14 +5,14 @@ import subprocess
 import psutil
 
 def temperatura():
-    #Obtém informação da frequ~encia do processador
-    url = "cat /sys/class/thermal/thermal_zone0/temp"
+    #Obtém informação da frequência do processador
+    url = ""
     temp = subprocess.check_output(url, shell=True)
     temp = float(temp) / 1000
     return temp
 
 def cpu():
-    #Obtém informação da frequ~encia do processador
+    #Obtém informação da frequência do processador
     uso_cpu = psutil.cpu_percent()
     return uso_cpu
 
