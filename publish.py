e6d5a0b2e9e36dd479487629605fefc8bebf7ad9
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-04-26 22:09:19 -0300

    loop for

--- publish.py
+++ publish.py
@@ -10,13 +10,25 @@ client.connect('broker.mqtt-dashboard.com', 1883, 60)
 
 try:
     while True:
-        mensagem = "Teste M"
+        mensagem = ""
+        for i in range(0,11):
+            mensagem = str(i)
+            payload = mensagem.encode('utf-8')
+            client.publish(TOPICO, payload, qos=0)
+            payload = payload.decode()
+            print(f'Mensagem: {TOPICO} {payload}')
+            sleep(5)
+        mensagem = "Fim do For"            
         payload = mensagem.encode('utf-8')
         client.publish(TOPICO, payload, qos=0)
         payload = payload.decode()
         print(f'Mensagem: {TOPICO} {payload}')
         sleep(5)
+        
 
 except KeyboardInterrupt:
     print("\n Crtl+C pressionado, encerrando aplicação e saindo...") # Sair do programa 
     sys.exit(0)
+
+
+
